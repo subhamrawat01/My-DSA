4ea6db6d466f0cf298240083a1acd7e65a38c363
subhamrawat01 <SHUBHAMSINGHRAWAT99@GMAIL.COM>
2024-10-17 00:35:36 +0530

    added pattern 9 and 10

--- Basics/PatternPrinting.java
+++ Basics/PatternPrinting.java
@@ -69,9 +69,38 @@ public class PatternPrinting{
             star-=2;
         }
     }
+    void pattern9(int n){
+        int space1 = n-1, star = 1, space2 = n-1;
+        for(int i =0; i<n; i++){
+            for(int j = 0; j<space1; j++) System.out.print(" ");
+            for(int j =0; j<star; j++) System.out.print("*");
+            for(int j = 0; j<space2; j++) System.out.print(" ");
+            System.out.println();
+            star += 2;
+            space1--;
+            space2--;
+        }
+        space1 = 0; star = 2*n-1; space2 = 0;
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<space1; j++) System.out.print(" ");
+            for(int j = 0; j<star; j++) System.out.print("*");
+            for(int j = 0; j<space2; j++) System.out.print(" ");
+            System.out.println();
+            star -= 2;
+            space1++;
+            space2++;
+        }
+    }
+    void pattern10(int n){
+        for(int i =0; i<2*n-1;i++){
+            int stars = i<n?i+1:2*n-1-i;
+            for(int j = 0; j<stars; j++) System.out.print("*");
+            System.out.println();
+        }
+    }
     public static void main(String[] args){
         PatternPrinting obj = new PatternPrinting();
-        obj.pattern8(5);
+        obj.pattern10(5);
         
     }
 }
